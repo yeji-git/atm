f606dd54cf59821641fc30605fa21983bfef4a17
yeji-git <ye-end@daum.net>
2023-03-14 13:05:00 +0900

    Create project ATM

---
+++ atm/src/atm/UserManager.java
@@ -0,0 +1,16 @@
+package atm;
+
+import java.util.ArrayList;
+
+public class UserManager {
+
+	private static ArrayList<User> list = new ArrayList<User>();
+	
+	// User ¿¡ ´ëÇÑ
+	
+	// C
+	// R
+	// U
+	// D
+	
+}
